d7695fcb17e82fb2721b4576fb51ca2939adb052
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2023-02-05 19:09:54 +0500

    masalar

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//function a(num) {
+//  let arr = num.split("");
+//  if (arr.reverse().join("") == num) {
+//    return true;
+//  }
+//  return false;
+//}
+//console.log(a("2002"));
+
+////////////////////////////*************** */
+
+//function kabisa(son) {
+//  if ((son % 4 == 0 && son % 100 != 0) || son % 400 == 0) {
+//    return "bu kabisa yili";
+//  }
+//  return "bu kabisa yili emas";
+//}
+//console.log(kabisa(8));
+
+///////////////////************************* */
+
+//function son(arr) {
+//  return arr.filter((son) => son < 0);
+//}
+//console.log(son([7, 2, -3, 4, -8, -48, 56]));
